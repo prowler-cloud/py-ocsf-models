4921c4f6bd64e05fd87bde422c1ec38b152afca6
Pepe Fagoaga <pepe@prowler.com>
2024-02-16 13:38:32 +0100

    chore: improve service description

--- py-ocsf-models/objects/service.py
+++ py-ocsf-models/objects/service.py
@@ -10,10 +10,10 @@ class Service(BaseModel):
     a way to uniquely identify and describe services.
 
     Attributes:
-    - labels (Optional[list[str]]): A list of labels or tags associated with the service, providing additional metadata.
-    - name (str): The name of the service, clearly identifying it within the system.
-    - uid (str): A unique identifier for the service, ensuring distinct recognition.
-    - version (str): The version of the service, helping to track changes or updates over time.
+    - Labels (labels) [Optional]: A list of labels or tags associated with the service, providing additional metadata.
+    - Name (name) [Optional]: The name of the service, clearly identifying it within the system.
+    - Unique ID	 (uid) [Optional]: A unique identifier for the service, ensuring distinct recognition.
+    - Version (version) [Optional]: The version of the service, helping to track changes or updates over time.
     """
 
     labels: Optional[list[str]]
